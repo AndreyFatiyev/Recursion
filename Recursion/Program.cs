34a4257261e559e081294ccd0c23067b4140b34a
AndreyFatiyev <andreyfatiyev@gmail.com>
2021-04-26 14:01:51 +0400

    Recursion

---
+++ Recursion/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            int sum = 0;
+            int degr = 1;
+            int countSum = 0;
+             countSum = Degree(x, sum, degr, countSum);
+            Degree(x,sum,degr, countSum);
+            Console.WriteLine($"summary is = {countSum}");
+
+        }
+        static int Degree(int x,int sum,int degr, int countSum)
+        {
+            degr *= 2;
+            sum = degr;
+            sum += degr;
+            countSum += sum;
+
+            if (x == 10)
+            {
+              
+                Console.WriteLine($"{x}  = x and {sum} = sum");
+                return countSum;
+                
+            }
+            else
+            {
+               
+                Console.WriteLine($"{x}  = x  and {sum } = sum");                
+                return Degree(x + 1,sum,degr, countSum);
+            }
+        }
+
+    }
+    }
+
